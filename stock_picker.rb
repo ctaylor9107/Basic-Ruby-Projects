269321ea2bc826a8d191f9598d793746a268d894
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-04-28 15:00:13 -0500

    Create Basic Logic for Stock Picker
    Created the basic logic for the stock_picker method.
    
    This takes a value in the given array and iterates over all the other
    values in the array to subtract the values.  It then creates a has
    containing:
    1. a two value array with the indices of the two values from the array
    2. the different from the two values
    
    The method currently iterates over all values, even values that come
    before it in the array (which would not be possible with consecutive
    days in real life), so the next step is to update so that it only
    compares with values that come after it in the array.

---
+++ stock_picker.rb
@@ -0,0 +1,13 @@
+def stock_picker(stock_array)
+
+  day_range_and_amount = {}
+
+  stock_array.each_with_index do |number, index|
+    i = 0
+    until i == stock_array.length
+      day_range_and_amount[[index, i]] = (number - stock_array[i])
+      i += 1
+    end
+  end
+  p day_range_and_amount
+end

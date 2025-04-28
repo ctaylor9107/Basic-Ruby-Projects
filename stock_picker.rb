a74e893aae39d744bcb0824fa86ede66df1ad7d6
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-04-28 15:11:59 -0500

    Modify Method to Only Look at Future Values
    The method has been updated so that it only compares itself against
    values that come after it in the array

--- stock_picker.rb
+++ stock_picker.rb
@@ -3,7 +3,7 @@ def stock_picker(stock_array)
   day_range_and_amount = {}
 
   stock_array.each_with_index do |number, index|
-    i = 0
+    i = (index + 1)
     until i == stock_array.length
       day_range_and_amount[[index, i]] = (number - stock_array[i])
       i += 1

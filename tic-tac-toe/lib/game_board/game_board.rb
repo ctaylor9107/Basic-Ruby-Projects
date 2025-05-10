23becb75d5d82579cabaa207fd7f827207e03312
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-05-09 19:36:57 -0500

    Add the Update Board method to the Class
    This method will be used by the game loop to update the tic-tac-toe grid
    with the player's choice on their specified row and column.

--- tic-tac-toe/lib/game_board/game_board.rb
+++ tic-tac-toe/lib/game_board/game_board.rb
@@ -1,6 +1,8 @@
 require_relative 'game_board_row'
 
 class GameBoard
+  attr_reader :game_board
+
   def initialize
     top_row = GameBoardRow.new("top")
     middle_row = GameBoardRow.new("middle")
@@ -13,7 +15,16 @@ class GameBoard
     ]
   end
 
-  def display
-    @game_board.each { |value| puts value }
+
+  def update_board(player_value, row, column)
+    
+    if @game_board[row][column] == " " || @game_board[row][column] == "_"
+      @game_board[row][column] = player_value
+      puts @game_board
+    else
+      puts "Pick a different spot"
+      puts @game_board
+      return "dumb"
+    end
   end
 end

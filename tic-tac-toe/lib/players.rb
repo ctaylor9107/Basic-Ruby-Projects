5345a520987545e39d36742b23e06ef3212307ca
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-05-09 19:35:00 -0500

    Add Row and Column Choice for Players
    Allow the players to choose a row and column in the tic-tac-toe grid.
    These methods will be used to allow the players to select where they
    want to add their value (x or o).

--- tic-tac-toe/lib/players.rb
+++ tic-tac-toe/lib/players.rb
@@ -5,4 +5,41 @@ class Player
     @player = player
     @value = value
   end
+
+  def player_row(view_gameboard_method)
+    puts "\nInput row: Top, Middle, or Bottom\n"
+    row_choice = gets.chomp
+  
+    if row_choice.downcase == "top"
+      0
+    elsif row_choice.downcase == "middle"
+      1
+    elsif row_choice.downcase == "bottom"
+      2
+    else
+      puts "\nInvalid input, try again\n"
+      puts view_gameboard_method
+      player_row(view_gameboard_method)
+    end
+  end
+
+  def player_column(view_gameboard_method)
+    puts "\nInput column: Left, Middle, or Right\n"
+    column_choice = gets.chomp
+  
+    if column_choice.downcase == "left"
+      0
+    elsif column_choice.downcase == "middle"
+      2
+    elsif column_choice.downcase == "right"
+      4
+    else
+      puts "\nInvalid input, try again\n"
+      puts view_gameboard_method
+      player_column(view_gameboard_method)
+    end
+  end
+
+
+
 end

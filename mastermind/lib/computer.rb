963ed639b73fc0b6b769bc25ec5b79e1fcdf50cb
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-05-10 14:38:30 -0500

    Create Computer Secret Code Method
    Created a method that will allow the computer to randomly determine the
    colors that it would like to use in its secret code.
    
    This will then create an array with four selected (and multiples can be
    selected) that the player will then have to guess.

--- mastermind/lib/computer.rb
+++ mastermind/lib/computer.rb
@@ -3,3 +3,14 @@ class Computer
   def initialize(computer)
     @computer = computer
   end
+
+  
+  def secret_code(color_list)
+    computer_secret_code = []
+    while computer_secret_code.length < 4
+      num = rand(6)
+      computer_secret_code.push(color_list[num])
+    end
+    puts computer_secret_code
+  end
+end

e5427d218f4f4303664cb2d2b320d7a85bae3ded
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-05-17 20:17:33 -0500

    Create Word Object
    Created the class that the secret word object will fall under.
    
    This picks a secret word from the words.txt file with a length between
    5 and 12.
    
    Spaces are also created based on the length of this word so the player
    will be able to guess the letters.

--- hangman/main.rb
+++ hangman/main.rb
@@ -1,7 +1,15 @@
 require_relative 'lib/hangman.rb'
+require_relative 'lib/word.rb'
 
 hangman = Hangman.new
+word_to_guess = Word.new('word')
 
-hangman_call =hangman.hangman_array
+secret_word = word_to_guess.secret_word
+letters = word_to_guess.blank_spaces(secret_word)
+hangman_call = hangman.hangman_array
 
 puts hangman_call[6]
+puts ""
+puts secret_word
+puts letters.join(" ")
+puts ""

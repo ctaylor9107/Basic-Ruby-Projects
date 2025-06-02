b0fcdc9a6249f136cd6a3e0c6aa83cc1ee8da980
ctaylor9107 <coleman.raphael.taylor@gmail.com>
2025-06-01 21:05:51 -0500

    Add Linked List Class
    Added the Linked List class to be able to access methods from that class
    for specific entries in the Hash Map array.
    
    Updated the existing Hash Map methods to access these methods as
    necessary.

--- hash_map/lib/hash_map.rb
+++ hash_map/lib/hash_map.rb
@@ -1,3 +1,6 @@
+require_relative 'node.rb'
+require_relative 'linked_list.rb'
+
 # Class for the Hash Map Data Structure
 class HashMap
   attr_reader :buckets
@@ -5,30 +8,60 @@ class HashMap
   def initialize(load_factor, capacity)
     @load_factor = load_factor
     @capacity = capacity
-    @buckets = Array.new(capacity, '')
+    @buckets = Array.new(capacity)
+    i = 0
+    until i == capacity
+      @buckets[i] = LinkedList.new
+      i += 1
+    end
   end
 
   def hash(key)
    hash_code = 0
    prime_number = 31
-      
+   
    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
-      
-   hash_code
+   
+   hash_code % @buckets.length
   end
 
   def set(key, value)
-    hasher = hash(key) % 16
-    @buckets[hasher] = [key, value]
+    hasher = hash(key)
+    @buckets[hasher].append([key, value])
   end
 
   def get(key)
     @buckets.each do |entry|
-      if entry[0] == key
-        return entry[1]
+      if entry.contains?(key)
+        return entry.get_value(key, entry.size)                                                        
       end
     end
     nil
   end
 
+  def has?(key)
+    selection = @buckets.select { |entry| entry.head != nil}
+    selection.each do |entry|
+      entry.contains?(key) ? (return true) : false
+    end
+    false
+  end
+
+  def entries
+    entries = []
+    i = 0
+    while i < @buckets.length
+      if @buckets[i].head == nil
+        i += 1
+      elsif @buckets[i].head != nil
+        curr_node = @buckets[i].head
+        until curr_node == nil
+          entries.push(curr_node.value)
+          curr_node = curr_node.next_node
+        end
+        i += 1
+      end
+    end
+    entries
+  end
 end
--- hash_map/main.rb
+++ hash_map/main.rb
@@ -1,10 +1,30 @@
 require_relative 'lib/hash_map.rb'
+require_relative 'lib/node.rb'
+require_relative 'lib/linked_list.rb'
 
 test = HashMap.new(0, 16)
 
-p test.buckets
 
 test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
 
-p test.buckets
+puts test.buckets.select { |entry| entry.head != nil }
+
+puts test.has?('apple')
+
+puts test.get('ice cream')
+
+p test.entries
+
+
 
